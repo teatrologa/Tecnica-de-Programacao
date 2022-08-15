ed882ab2b4244219a63f953d447c19ceb3489bcc
Marília Castro <marilialiliu@hotmail.com>
2022-08-15 15:15:14 -0300

    Exercicios de lista feitos +  usando linq.

--- Exercicios - Aula 04/Program.cs
+++ Exercicios - Aula 04/Program.cs
@@ -27,7 +27,7 @@ using System.Linq;
 //Console.WriteLine(" ");
 //foreach (int i in numerosInt)
 //{
-    
+
 //    if (i == numero)
 //    {
 //        Console.WriteLine($"O numero {numero} aparece no indice: {roll}");
@@ -46,7 +46,7 @@ using System.Linq;
 //bool val = false;
 //do
 //{
-    
+
 //    Console.WriteLine("Crie uma lista de números inteiros. Siga o formato:\n    1, 2, 3, 4 ...\nDê 'Enter' quando ela estiver acabada.");
 //    nums = Console.ReadLine();
 
@@ -60,7 +60,7 @@ using System.Linq;
 //    {
 //        val = true;
 //    }
-    
+
 
 //} while (val == false);
 
@@ -73,7 +73,7 @@ using System.Linq;
 
 //foreach (int i in listInt)
 //{
-    
+
 //    if (i > 0)
 //    {
 //        contador++;
@@ -91,18 +91,59 @@ using System.Linq;
 
 
 
-//Ex2
-List<int> valores = new List<int>();
-string valString;
-int maiorValor, menorValor, resultado;
-bool validacaoInput = false;
+////Ex2
+//List<int> valores = new List<int>();
+//string valString;
+//int maiorValor, menorValor, resultado;
+//bool validacaoInput = false;
+
+//do
+//{
+//    Console.WriteLine("Crie uma lista de números inteiros. Siga o formato:\n    1, 2, 3, 4 ...\nDê 'Enter' quando ela estiver acabada.");
+//    valString = Console.ReadLine();
+
+//    if (String.IsNullOrEmpty(valString))
+//    {
+//        Console.WriteLine("Entrada inválida, digite 1 ou mais numeros inteiros [ex: -5, 1, 2, 3...]\nDê enter e tente novamente.");
+//        Console.ReadKey();
+//        Console.Clear();
+//    }
+//    else
+//    {
+//        validacaoInput = true;
+//    }
+
+//} while (validacaoInput == false);
+
+
+//foreach (var v in valString.Split(","))
+//{
+//    //valores.Add(int.Parse(v));
+//    valores.Add(Convert.ToInt32(v));
+//};
+
+//maiorValor = valores.Max();
+//menorValor = valores.Min();
+//resultado = (maiorValor) - (menorValor);
+
+//Console.WriteLine($"A diferença entre o maior valor ({maiorValor}) e o menor valor ({menorValor}) da sua lista é: {resultado}.");
+
+
+
+
+//Ex3
+List<int> numList = new List<int>();
+string numString;
+bool valInput = false;
+int zero = 0, indice = 0, resultBool;
 
 do
 {
     Console.WriteLine("Crie uma lista de números inteiros. Siga o formato:\n    1, 2, 3, 4 ...\nDê 'Enter' quando ela estiver acabada.");
-    valString = Console.ReadLine();
+    numString = Console.ReadLine();
 
-    if (String.IsNullOrEmpty(valString))
+
+    if (String.IsNullOrEmpty(numString))
     {
         Console.WriteLine("Entrada inválida, digite 1 ou mais numeros inteiros [ex: -5, 1, 2, 3...]\nDê enter e tente novamente.");
         Console.ReadKey();
@@ -110,26 +151,29 @@ do
     }
     else
     {
-        validacaoInput = true;
+        valInput = true;
     }
 
-} while (validacaoInput == false);
 
+} while (valInput == false) ;
 
-foreach (var v in valString.Split(","))
+foreach (var v in numString.Split(","))
 {
-    //valores.Add(int.Parse(v));
-    valores.Add(Convert.ToInt32(v));
+    numList.Add(Convert.ToInt32(v));
 };
 
-maiorValor = valores.Max();
-menorValor = valores.Min();
-resultado = (maiorValor) - (menorValor);
-
-Console.WriteLine($"A diferença entre o maior valor ({maiorValor}) e o menor valor ({menorValor}) da sua lista é: {resultado}.");
+int numeroProxZero = numList.Aggregate((x, y) => Math.Abs(x - zero) < Math.Abs(y - zero) ? x : y);
+//Console.WriteLine(numeroProxZero);
 
 
 
 
-//Ex3
+if (numList.Count(x => x == numeroProxZero) > 1)
+{
+    Console.WriteLine("Há 2 ou mais valores equidistrantes de zero (0), logo não há nenhum resultado.");
+}
+else
+{
 
+    Console.WriteLine($"Dentre os numeros da lista, o que mais se aproxima de zero (0) é: {numeroProxZero}");
+}
