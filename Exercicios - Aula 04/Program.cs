ceb63b9116ac4a1efda857a48f438162fbcd74e7
Marília Castro <marilialiliu@hotmail.com>
2022-08-15 11:27:50 -0300

    Eninciados colocados + Ex ArrayList feito

---
+++ Exercicios - Aula 04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//Exercicio de ArrayList
+
+ArrayList numerosInt = new ArrayList();
+ArrayList numerosString = new ArrayList();
+int numero;
+string numeros;
+
+Console.WriteLine("Crie uma lista de números inteiros. Siga o formato:\n    1, 2, 3, 4 ...\nDê 'Enter' quando ela estiver acabada.");
+numeros = Console.ReadLine();
+numerosString.AddRange(numeros.Split(","));
+foreach (string str in numerosString)
+{
+    numero = Convert.ToInt32(str);
+    numerosInt.Add(numero);
+}
+
+Console.WriteLine("Agora, vamos buscar por um numero dentro desta lista. Digite-o abaixo: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+int roll = 0;
+
+Console.WriteLine(" ");
+foreach (int i in numerosInt)
+{
+    
+    if (i == numero)
+    {
+        Console.WriteLine($"O numero {numero} aparece no indice: {roll}");
+    };
+    roll++;
+}
+
+
+//Exercicios de List
+//Ex1
