b58c0eb50a931e02b5f0b033884063020209a724
Marília Castro <marilialiliu@hotmail.com>
2022-08-18 20:49:34 -0300

    Exercicios prontinhos + visualizações feitas Aula 06 atualizada

---
+++ Aula 06/Program.cs
@@ -0,0 +1,192 @@
+﻿//Aula 06 de Técnicas de Porgramação
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Aula_06
+{
+    class Program
+    {
+        private static List<Produto> listaProdutos = new()
+        {
+            new Produto { Id = 2, Nome = "Camiseta", Valor = 52, Ativo = true},
+            new Produto { Id = 8, Nome = "Guarda-Chuva", Valor = 19, Ativo = true},
+            new Produto { Id = 4, Nome = "Celular", Valor = 8500, Ativo = true },
+            new Produto { Id = 3, Nome = "Arroz", Valor = 21, Ativo = true },
+            new Produto { Id = 1, Nome = "Geladeira", Valor = 1900, Ativo = true },
+            new Produto { Id = 9, Nome = "Panela", Valor = 41, Ativo = true },
+            new Produto { Id = 5, Nome = "Chinelo", Valor = 11, Ativo = true },
+            new Produto { Id = 7, Nome = "TV", Valor = 2350, Ativo = true },
+            new Produto { Id = 6, Nome = "Patins", Valor = 66, Ativo = true },
+            new Produto { Id = 10, Nome = "Espelho", Valor = 115, Ativo = true },
+        };
+
+        static void Main(string[] args)
+        {
+            var retorno = RetornaPrimeiroProduto();
+            if (retorno != null)
+            {
+                WriteLine(retorno.ToString());
+            }
+            else { WriteLine(retorno); }
+
+            
+            WriteLine("");
+
+
+            var ret = RetornoListaOrdenadaTop3();
+            foreach(Produto prod in ret)
+            {
+                WriteLine(prod.ToString());
+            }
+
+
+            WriteLine("");
+
+
+            var rtn = RetornaProdutoDto();
+            foreach (ProdutoDto prodDto in rtn)
+            {
+                WriteLine(prodDto.ToString());
+            }
+
+
+            static Produto RetornaPrimeiroProduto()
+            {
+                return listaProdutos.FirstOrDefault();
+            }
+
+            static Produto RetornarUltimoProduto()
+            {
+                return listaProdutos.LastOrDefault();
+            }
+
+            static Produto RetornarUnicoProduto()
+            {
+                return listaProdutos.SingleOrDefault(prod => prod.Id == 2);
+            }
+
+            static List<Produto> RetornaProdutoInicioLetra()
+            {
+                return listaProdutos.Where(prod => prod.Nome.Substring(0, 1).ToLower() == "a".ToLower()).ToList();
+                    //Dentro do substring o 0 é  indicice primeiro e o 1 é a quantidade de 
+                    // caractere que vou comparar, no caso só a primeira letra.
+            }
+
+            static List<Produto> RetornarProdutoUltimaLetra()
+            {
+                return listaProdutos.Where(prod => prod.Nome.EndsWith('o')).ToList();
+                //o VS sugere que após o EndsWith tenha o .Equals(true)
+                // porém não tenho certeza do que ele faz
+            }
+
+            static List<Produto> RetornoListaOrdenada()
+            {
+                return listaProdutos.OrderBy(prod => prod.Id).ToList(); //Ordenação simples com 1 coluna a ser ordenada
+            }
+
+            static List<Produto> RetornoListaSuperOrdenada()
+            {
+                return listaProdutos.OrderBy(prod => prod.Nome).ThenBy(prod => prod.Valor).ThenBy(prod => prod.Id).ToList();
+                //agora sim podemos ordenar por mais de 1 coluna
+                //como ordenar pelo titulo de filme de acordo com seu ano de lançamento (ano + titulo)
+                //Pode ser colocado varios ThenBy contanto que tenha um OrderBy antes
+            }
+
+            static List<ProdutoDto> RetornaProdutoDto()
+            {
+                //O select serve para filtrar entre os campos de uma classe "mãe"
+                // ai se vc quiser pegar mais de uma coluna, vc faz uma classe DTO
+                // filtrando as colunas que vc quer pegar da mãe.Imagine isso no SQN (os selects)
+                return listaProdutos.Select(prod => new ProdutoDto()
+                {
+                    NomeProduto = prod.Nome,
+                    ValorProduto = prod.Valor
+
+                }).ToList();
+            }
+
+            static string RetornaPrimeiroNome()
+            {
+                return listaProdutos.Select(p => p.Nome).FirstOrDefault();
+                //O retorno já é uma string, logo, lá em cima no writeLine não preciso colocar o ToString().
+                //O Select nesse caso seleciona apenas as info de 1 coluna, não precisando da classe DTO do produto
+                // e se vc quiser a lista disso é só criar o método lista dele.
+            }
+
+            static List<Produto> RetornaProdutoInicioLetraListaOrdenada()
+            {
+                return listaProdutos
+                    .Where(prod => prod.Nome.Substring(0, 1).ToLower() == "c".ToLower())
+                    .OrderBy(prod => prod.Id)
+                    .ToList();
+                //Aqui só estou mostrando que posso escrever/identar dessa maneira quando as coisas ficam
+                // grandes demais para colocar em 1 linha apenas
+                // dessa forma também fica bastante visível o que está acontecendo.
+            }
+
+            var val = listaProdutos.Min(prod => prod.Valor); // pega o menor valor
+            var vId = listaProdutos.Max(prod => prod.Id); //pega o maior ID
+            var vNome = listaProdutos.Min(n => n.Nome); //vai seguir a ordem alfabetica
+
+            Write("\nMin valor: ");
+            WriteLine(val);
+            Write("Min nome: ");
+            WriteLine(vNome);
+            Write("Max ID: ");
+            WriteLine(vId);
+
+            //O Average calcula a média do que quer que você coloque ali (numerico)
+            var media = listaProdutos.Average(prod => prod.Valor);
+            Write("Media de valores de todos os produtos da lista: ");
+            WriteLine(media);
+
+            static List<Produto> RetornoListaOrdenadaTop3()
+            {
+                return listaProdutos.OrderBy(prod => prod.Id).Take(3).ToList();
+                //O ".Take(3)" ou qualquer número, dá pra usar aninhado, e é interessante
+                //pq ele traz os topN itens que vc deseja.
+            }
+
+            //O Any() traz um booleano que diz se o que está dentro do lambda é verdadeiro.
+            var tst = listaProdutos.Any(prod => prod.Ativo == false);
+            WriteLine("");
+            WriteLine(tst);
+            //Se usar o Any() vazio, ele retorna, por ex se existe algum elemento dentro da lista ou não etc 
+
+
+            
+
+        }
+
+
+        public class Produto
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public decimal Valor { get; set; }
+            public bool Ativo { get; set; }
+
+            public override string ToString()
+            {
+                return $"Id: {Id} | Nome: {Nome} | Valor: R$ {Valor}";
+            }
+        }
+
+        public class ProdutoDto
+        {
+            public string NomeProduto { get; set; }
+
+            public decimal ValorProduto { get; set; }
+
+            public override string ToString()
+            {
+                return $"Nome: {NomeProduto} | Valor: {ValorProduto}";
+            }
+        }
+    }
+}
--- Exercicios - Aula 06/Program.cs
+++ Exercicios - Aula 06/Program.cs
@@ -14,20 +14,166 @@ namespace Aula_06_Exercicios
             new Produto { Id = 2, Nome = "Camiseta", Valor = 52, Ativo = true},
             new Produto { Id = 8, Nome = "Guarda-Chuva", Valor = 19, Ativo = true},
             new Produto { Id = 4, Nome = "Celular", Valor = 8500, Ativo = true },
-            new Produto { Id = 3, Nome = "Arroz", Valor = 21, Ativo = true },
+            new Produto { Id = 3, Nome = "Arroz", Valor = 21, Ativo = false },
             new Produto { Id = 1, Nome = "Geladeira", Valor = 1900, Ativo = true },
             new Produto { Id = 9, Nome = "Panela", Valor = 41, Ativo = true },
-            new Produto { Id = 5, Nome = "Chinelo", Valor = 11, Ativo = true },
+            new Produto { Id = 5, Nome = "Chinelo", Valor = 11, Ativo = false },
             new Produto { Id = 7, Nome = "TV", Valor = 2350, Ativo = true },
             new Produto { Id = 6, Nome = "Patins", Valor = 66, Ativo = true },
+            new Produto { Id = 10, Nome = "Espelho", Valor = 115, Ativo = true },
         };
         
 
-        static void Master(string[] args)
+        static void Main(string[] args)
         {
+            //Visualizações do método 1:
+            //WriteLine("Busque produtos de acordo com a ultima letra do seu nome\nPor exemplo, digite: o");
+            //char input = Convert.ToChar(ReadLine());
+            char input = 'o';
+            var ret = RetornarProdutoUltimaLetra(input);
+            if (ret.Count() > 1)
+            {
+                WriteLine("Os produtos cuja ultima letra é 'o' são:");
+                foreach (Produto prod in ret)
+                {
+                    WriteLine(prod.ToString());
+                }
+            } else if (ret.Count == 1)
+            {
+                WriteLine("O produto cuja ultima letra é 'o' é:");
+                foreach (Produto prod in ret)
+                {
+                    WriteLine(prod.ToString());
+                }
+            }
+            else { WriteLine("Não há nenhum produto cuja ultima letra é 'o'."); }
+            
+            WriteLine("");
 
+            //Visualização do método 2:
+            var valor = RetornarValorMenor50();
+            WriteLine("Lista de produtos com valor abaixo de 50:"); //Preguiça de fazer os if como os de cima. Mas é possível.
+            foreach(Produto v in valor)
+            {
+                WriteLine(v.ToString());
+            }
 
+            WriteLine("");
+
+            //Visualização do método 3:
+            var media = RetornarMediaProdInativos();
+            Write("A média de valor dos produtos inativos é: ");
+            WriteLine(media.ToString());
+
+            WriteLine("");
+
+            //Visualização do método 4:
+            var retorno = PrimeiroProdutoMenor10();
+            Write("O primeiro produto inserido no sistema cujo valor é inferior a 10 é: ");
+            if (retorno != null)
+            {
+                WriteLine(retorno.ToString());
+            }
+            else { WriteLine($"{retorno} - Não existe"); }
+
+            WriteLine("");
+
+            //Visualização do método 5:
+            var num = UltimoProdutoOrdemDecrescente();
+            WriteLine("O último produto em ordem alfabética decrescente é: ");
+            if (num != null)
+            {
+                WriteLine(num.ToString());
+            }
+            else { WriteLine(num); }
+
+            WriteLine("");
+
+            //Visualização do método 6:
+            Write("Insira o indice do produto que você deseja alterar o valor: ");
+            int id = Convert.ToInt32(ReadLine());
+            Write("Insira o novo valor do produto: ");
+            decimal newValor = Convert.ToDecimal(ReadLine());
+
+            if (listaProdutos.Any(p => p.Id == id))
+            {
+                AtualizarValorProduto(id, newValor);
+                var produtinho = RetornarProdutoIndice(id);
+                WriteLine($"O produto {produtinho.Nome} foi atualizado com sucesso!");
+                WriteLine(produtinho.ToString());
+            }
+            else
+            {
+                WriteLine($"Foi impossível encontrar o produto de id: {id}");
+            }
 
         }
+
+
+
+        //Exercício 1 - Método 1
+        public static List<Produto> RetornarProdutoUltimaLetra(char input)
+        {
+            return listaProdutos.Where(prod => prod.Nome.EndsWith(input)).ToList();
+            //o VS sugere que após o EndsWith tenha o .Equals(true)
+            // porém não tenho certeza do que ele faz
+        }
+
+        //Exercício 2 - Método 2
+        public static List<Produto> RetornarValorMenor50()
+        {
+            return listaProdutos.Where(prod => prod.Valor < 50).ToList();
+        }
+
+        //Exercício 3 - Método 3
+        public static decimal RetornarMediaProdInativos()
+        {
+            return listaProdutos.Where(p => p.Ativo == false).Average(p => p.Valor);
+        }
+
+        //teste mal sucedido do exercício 3, caso não haja nenhum item inativo
+        public static decimal RetornarMediaProdInativo()
+        {
+            var teste = listaProdutos.Where(p => p.Ativo == false);
+            if (teste != null)
+            {
+                return listaProdutos.Where(p => p.Ativo == false).Average(p => p.Valor);
+            }
+            else { return 0; }
+        }
+
+        //Exercício 4 - Método 4
+        public static Produto PrimeiroProdutoMenor10()
+        {
+            return listaProdutos.Where(p => p.Valor < 10).FirstOrDefault();
+        }
+
+        //Exercício 5 - Método 5
+        public static Produto UltimoProdutoOrdemDecrescente()
+        {
+            return listaProdutos.OrderByDescending(p => p.Nome).LastOrDefault();
+        }
+
+        //Exercício 6.1 - Método 6.1
+        public static void AtualizarValorProduto(int id, decimal valor)
+        {
+            //Seleciono o produto que quero aqui, de acordo com seu indice.
+            Produto produto = listaProdutos.Where(p => p.Id == id).Select(p => p).FirstOrDefault();
+
+            if (produto != null)
+            {
+                produto.Valor = valor;
+                //return produto.ToString();
+            }
+
+            //return $"Não há nenhum produto com o indice {id}";
+        }
+
+        //Exercício 6.2 - Método 6.2
+        public static Produto RetornarProdutoIndice(int id)
+        {
+            return listaProdutos.SingleOrDefault(p => p.Id == id);
+        }
+
     }
 }
