a4630c06996878029d62617ca76b322ad6f60311
Marília Castro <marilialiliu@hotmail.com>
2022-08-17 21:38:36 -0300

    inserindo info de aula

--- Aula 06/Program.cs
+++ Aula 06/Program.cs
@@ -9,18 +9,18 @@ using static System.Console;
 namespace Aula_06
 {
 
-    //private static List<Produto> listaProdutos = new()
-    //{
-    //    new Produto{Id = 2, Nome = "Camiseta", Valor = 52, Ativo = true},
-    //    new Produto{Id = 8, Nome = "Guarda-Chuva", Valor = 19, Ativo = true},
-    //    new Produto{Id = 4, Nome = "Celular", Valor = 8500, Ativo = true},
-    //    new Produto{Id = 3, Nome = "Arroz", Valor = 21, Ativo = false},
-    //    new Produto{Id = 1, Nome = "Geladeira", Valor = 1900, Ativo = true},
-    //    new Produto{Id = 9, Nome = "Panela", Valor = 41, Ativo = true},
-    //    new Produto{Id = 5, Nome = "Chinelo", Valor = 11, Ativo = false},
-    //    new Produto{Id = 7, Nome = "TV", Valor = 2350, Ativo = true},
-    //    new Produto{Id = 6, Nome = "Patins", Valor = 66, Ativo = true},
-    //};
+    private static List<Produto> listaProdutos = new()
+    {
+        new Produto{Id = 2, Nome = "Camiseta", Valor = 52, Ativo = true},
+        new Produto{Id = 8, Nome = "Guarda-Chuva", Valor = 19, Ativo = true},
+        new Produto{Id = 4, Nome = "Celular", Valor = 8500, Ativo = true},
+        new Produto{Id = 3, Nome = "Arroz", Valor = 21, Ativo = false},
+        new Produto{Id = 1, Nome = "Geladeira", Valor = 1900, Ativo = true},
+        new Produto{Id = 9, Nome = "Panela", Valor = 41, Ativo = true},
+        new Produto{Id = 5, Nome = "Chinelo", Valor = 11, Ativo = false},
+        new Produto{Id = 7, Nome = "TV", Valor = 2350, Ativo = true},
+        new Produto{Id = 6, Nome = "Patins", Valor = 66, Ativo = true},
+    };
 }
 
 
