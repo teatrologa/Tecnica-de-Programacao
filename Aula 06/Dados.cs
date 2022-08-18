f0d19a73563ef5ca22ab1616744c555831096492
Marília Castro <marilialiliu@hotmail.com>
2022-08-17 21:37:48 -0300

    criado a aula 06

---
+++ Aula 06/Dados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_06
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool Ativo { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Nome: {Nome} | Valor: R$ {Valor}";
+        }
+    }
+}
