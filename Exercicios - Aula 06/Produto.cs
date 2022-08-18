4d7fc4cef7328754a6ef0c82d1caa178d119ce1d
Marília Castro <marilialiliu@hotmail.com>
2022-08-17 22:02:09 -0300

    deletando aula 06

---
+++ Exercicios - Aula 06/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Aula_06_Exercicios
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool Ativo { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Nome: {Nome} | Valor: R$ {Valor}";
+        }
+    }
+}
